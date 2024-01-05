093c3153e54db6ebaa02641187fb7448008d12e6
stepanvalic <131240792+stepanvalic@users.noreply.github.com>
2024-01-05 17:58:35 +0100

    Update cs.lua

--- locales/cs.lua
+++ locales/cs.lua
@@ -1,58 +1,57 @@
---translate by stepan_valic
 local Translations = {
     error = {
-        no_vehicles = "V tomto místě nejsou žádná vozidla!",
-        not_impound = "Vaše vozidlo není v úschovně",
+        no_vehicles = "Na tomto místě momentálně nemáte žádná vozidla...",
+        not_impound = "Vaše vozidlo není v areálu",
         not_owned = "Toto vozidlo nelze uložit",
-        not_correct_type = "Tento typ vozidla nelze zde uložit",
-        not_enough = "Nedostatek peněz",
-        no_garage = "Žádný",
+        not_correct_type = "Tento typ vozidla zde nelze uložit",
+        not_enough = "Není dostatek peněz",
+        no_garage = "Žádné",
         vehicle_occupied = "Toto vozidlo nelze uložit, protože není prázdné",
     },
     success = {
-        vehicle_parked = "Vozidlo uloženo",
+        vehicle_parked = "Vozidlo je uloženo",
     },
     menu = {
         header = {
-            house_car = "Dům Garáž %{value}",
-            public_car = "Veřejná Garáž %{value}",
-            public_sea = "Veřejné Přístaviště %{value}",
-            public_air = "Veřejný Hangár %{value}",
-            job_car = "Pracovní Garáž %{value}",
-            job_sea = "Pracovní Přístaviště %{value}",
-            job_air = "Pracovní Hangár %{value}",
-            gang_car = "Gang Garáž %{value}",
-            gang_sea = "Gang Přístaviště %{value}",
-            gang_air = "Gang Hangár %{value}",
-            depot_car = "Depo %{value}",
-            depot_sea = "Depo %{value}",
-            depot_air = "Depo %{value}",
+            house_car = "Domácí garáž %{hodnota}",
+            public_car = "Veřejná garáž %{hodnota}",
+            public_sea = "Veřejná loděnice %{hodnota}",
+            public_air = "Veřejný hangár %{hodnota}",
+            job_car = "Pracovní garáž %{hodnota}",
+            job_sea = "Job Boathouse %{value}",
+            job_air = "Pracovní hangár %{hodnota}",
+            gang_car = "Gang Garage %{value}",
+            gang_sea = "Gang Boathouse %{value}",
+            gang_air = "Hangár gangu %{hodnota}",
+            depot_car = "Depot %{hodnota}",
+            depot_sea = "Depot %{hodnota}",
+            depot_air = "Depo %{hodnota}",
             vehicles = "Dostupná vozidla",
-            depot = "%{value} [ $%{value2} ]",
+            depot = "%{hodnota} [ $%{hodnota2} ]",
             garage = "%{value} [ %{value2} ]",
         },
         leave = {
-            car = "⬅ Odejít z Garáže",
-            sea = "⬅ Odejít z Přístaviště",
-            air = "⬅ Odejít z Hangáru",
+            car = "⬅ Leave Garage",
+            moře = "⬅ Opustit loděnici",
+            air = "⬅ Leave Hangar",
         },
         text = {
             vehicles = "Zobrazit uložená vozidla!",
-            depot = "SPZ: %{value}<br>Palivo: %{value2} | Motor: %{value3} | Karoserie: %{value4}",
-            garage = "Stav: %{value}<br>Palivo: %{value2} | Motor: %{value3} | Karoserie: %{value4}",
+            depot = "Deska: %{hodnota}<br>Palivo: %{hodnota2} | Motor: %{hodnota3} | Karoserie: %{hodnota4}",
+            garage = "Stav: %{hodnota}<br>Palivo: %{hodnota2} | Motor: %{hodnota3} | Karoserie: %{hodnota4}",
         }
     },
     status = {
-        out = "Venku",
-        garaged = "V Garáži",
+        out = "Out",
+        garaged = "Garaged",
         impound = "Zabaveno policií",
     },
     info = {
         car_e = "E - Garáž",
-        sea_e = "E - Přístaviště",
+        sea_e = "E - Loděnice",
         air_e = "E - Hangár",
-        park_e = "E - Uložit Vozidlo",
-        house_garage = "Dům garáž",
+        park_e = "E - Sklad vozidel",
+        house_garage = "Domácí garáž",
     }
 }
 
@@ -63,3 +62,4 @@ if GetConvar('qb_locale', 'en') == 'cs' then
         fallbackLang = Lang,
     })
 end
+--translate by stepan_valic
